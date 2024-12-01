cb40c3bee1afed03b3e4af3dd2782f7eaa1252d8
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2024-11-30 23:59:45 -0500

    feat: improved code
    Some code improvements

--- oop/0009/DivideByZero/src/Main.java
+++ oop/0009/DivideByZero/src/Main.java
@@ -1,24 +1,44 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Ingresa un numerador: ");
-        int numerador = Integer.parseInt(input.nextLine());
-        System.out.println("Ingresa un denominador: ");
-        int denominador = Integer.parseInt(input.nextLine());
+    public static void main(String[] args) {
+        try{
+            double numerator =  getNumerator();
+            double denominator =  getDenominator();
 
-        double resultado = obtenerDenominador(numerador, denominador);
+            double result = division(numerator, denominator);
 
-        System.out.println("El resultado es: " + resultado);
+            so
+
+        }catch (Exception e) {
+            System.out.println("");
+        }
     }
 
-    public static double obtenerDenominador(int numerador, int denominador) throws Exception{
-        try {
-            int resultado = numerador / denominador;
-            return resultado;
-        } catch (Exception e) {
-            throw new Exception("Ocurrió un error: " + e.getMessage());
+    public static double getNumerator() throws Exception{
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingresa un numerador: ");
+        int numerator = Integer.parseInt(input.nextLine());
+
+        if (numerator == 0){
+            throw new Exception("No puedes dividir por 0");
         }
+        return numerator;
+    }
+
+    public static double getDenominator() throws Exception{
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingresa un denominador: ");
+        int denominator = Integer.parseInt(input.nextLine());
+
+        if (denominator == 0){
+            throw new Exception("No puedes dividir por 0");
+        }
+        return denominator;
+    }
+
+    public static double division(double numerator, double denominator){
+        return numerator/ denominator;
     }
 }
