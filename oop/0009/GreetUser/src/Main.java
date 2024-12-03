a8cf66710841b6cf1ca9fe2afec91ec957a5731a
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2024-12-03 00:22:47 -0500

    feat: greetuser exception
    Finished code for exercise greetuser exception.

--- oop/0009/GreetUser/src/Main.java
+++ oop/0009/GreetUser/src/Main.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        String fullname;
+        try {
+            fullname = askNameLastname();
+            System.out.println("Saludos " + fullname);
+        } catch(Exception error) {
+            System.out.println("Error: " + error.getMessage());
+
+
+    }
+    }
+
+    public static String askNameLastname()  throws Exception {
+        Scanner input = new Scanner(System.in);
+        String name;
+        String lastname;
+        try {
+            System.out.println("Type your name: ");
+            name = input.nextLine();
+            if (name.length() <= 2) {
+                throw new Exception("El nombre debe tener más de dos caracteres.");
+            }
+            System.out.println("Type your Lastname: ");
+            lastname = input.nextLine();
+        } finally {
+            input.close();
+        }
+        return name + " " + lastname;
+    }
 }
