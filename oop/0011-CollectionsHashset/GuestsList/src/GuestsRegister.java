51ce756381a41c58764dba02995211b6e5493907
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2024-12-05 00:12:59 -0500

    feat: normalizeName Method, Validations for empty names, Better outputs
    Creation of normalizeName method, this way it can strip spaces and set everything to lowercase.
    
    New validation for empty inputs and empty list.
    
    Improved some output messages

--- oop/0011-CollectionsHashset/GuestsList/src/GuestsRegister.java
+++ oop/0011-CollectionsHashset/GuestsList/src/GuestsRegister.java
@@ -8,40 +8,49 @@ public class GuestsRegister {
         this.guestList = new HashSet<>();
     }
 
-    public void addGuest(String name){
+    public void addGuest(String name) {
         String normalizedName = normalizeName(name);
-        if(guestList.contains(normalizedName)){
-            System.out.println("El nombre ya está en la lista.");
+        if (normalizedName.isEmpty()) {
+            System.out.println("El nombre no puede estar vacío.");
         } else {
-            System.out.println("El invitado " + name + " ha sido agregado a la lista.");
-            guestList.add(normalizedName);
+            if (guestList.contains(normalizedName)) {
+                System.out.println("El nombre ya está en la lista.");
+            } else {
+                System.out.println("El invitado " + name + " ha sido agregado a la lista.");
+                guestList.add(normalizedName);
+            }
+        }
+
+    }
+
+    public void deleteGuest(String name) {
+        String normalizedName = normalizeName(name);
+        if (normalizedName.isEmpty()) {
+            System.out.println("El nombre no puede estar vacío.");
+        } else {
+            if (!guestList.contains(normalizedName)) {
+                System.out.println("El invitado " + name + " no existe en la lista.");
+            } else {
+                System.out.println("El invitado " + name + " ha sido removido de la lista.");
+                guestList.remove(normalizedName);
+            }
         }
     }
 
-    public void deleteGuest(String name){
-        String normalizedName = normalizeName(name);
-        if (!guestList.contains(normalizedName)){
-            System.out.println("El invitado " + name + " no existe en la lista.");
-        } else {
-            System.out.println("El invitado " + name + " ha sido removido de la lista.");
-            guestList.remove(normalizedName);
-        }
-    }
-
-    public void showGuests(){
-        if(guestList.isEmpty()){
+    public void showGuests() {
+        if (guestList.isEmpty()) {
             System.out.println("La lista está vacía");
-        } else{
-        Iterator<String> iterator = guestList.iterator();
-        int counter = 1;
-        System.out.println("*********LISTA DE INVITADOS*********");
-        while (iterator.hasNext()){
-            System.out.println(counter++ + ")" + iterator.next());
+        } else {
+            Iterator<String> iterator = guestList.iterator();
+            int counter = 1;
+            System.out.println("*********LISTA DE INVITADOS*********");
+            while (iterator.hasNext()) {
+                System.out.println(counter++ + ")" + iterator.next());
+            }
         }
     }
-    }
 
-    public String normalizeName(String name){
+    public String normalizeName(String name) {
         return name.strip().toLowerCase();
     }
 }
